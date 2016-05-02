1e937ad85f77067fb289a2744c8a585abb8149a6
edcvgy <edcvgy@users.noreply.github.com>
2016-05-03 01:33:58 +0530

    Initial checkin of Sales Tool

---
+++ SGit/src/main/java/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		SalesData obj = new SalesData();
+		
+		displayGreetings();
+		obj.dispaySalesData();
+		
+	}
+	
+	public static void displayGreetings()
+	{
+		System.out.println("HELLO HAPPY SALES PEOPLE!! ");
+		System.out.println("This app shows Sales Data");
+	}
+}
---
+++ SGit/src/main/java/SalesData.java
@@ -0,0 +1,8 @@
+
+public class SalesData {
+
+	public void dispaySalesData()
+	{
+		System.out.println("Displaying Sales Data. Please Wait .....");
+	}
+}
